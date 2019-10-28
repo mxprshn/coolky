75c9b3009b65ee21f79eeeb7b75ff001728ca060
Victoria <viky.fomina@yandex.ru>
2019-10-28 12:49:30 +0300

    added choice of types of dishes

--- tryToMakeSomeActivity/app/src/main/java/com/example/coolky/RecipesSearchFragment.kt
+++ tryToMakeSomeActivity/app/src/main/java/com/example/coolky/RecipesSearchFragment.kt
@@ -1,18 +1,21 @@
 package com.example.coolky
 
-
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import kotlinx.android.synthetic.main.fragment_recipes_search.*
 
 /**
  * A simple [Fragment] subclass.
  */
-class RecipesSearchFragment : Fragment() {
-
-    override fun onCreateView(
+public class RecipesSearchFragment : Fragment() {
+    public override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
@@ -20,5 +23,25 @@ class RecipesSearchFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_recipes_search, container, false)
     }
 
+    public override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
 
+        chooseTypeOfDish.setOnClickListener(this::typeOfDishClickHandler)
+    }
+
+    private fun typeOfDishClickHandler(typeOfDish: View) {
+       if (typeOfDish is Button) {
+            val builder = AlertDialog.Builder(this.context)
+            builder.setTitle(R.string.chooseTypeOfDishText)
+                .setItems(R.array.typesOfDishes, DialogInterface.OnClickListener { dialog, which ->
+                    
+                        // The 'which' argument contains the index position
+                        // of the selected item
+                    })
+                .create()
+                .show()
+       }
+    }
+
+    private var typeOfDish: String? = null
 }
